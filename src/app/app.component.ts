9a7bbccc78ec40ed896582a51d3e9322ac605b3a
Grace <wangxiaotian2012@gmail.com>
2022-08-09 21:03:50 -0700

    Add the name property to the AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-first-app';
+  title = 'angular-first-app 🧚🏻‍♀️';
+  name = 'Grace'
 }
