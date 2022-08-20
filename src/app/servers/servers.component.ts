d1b2138401ce5e3380e70eda24217845bd7ebfd7
Grace <wangxiaotian2012@gmail.com>
2022-08-19 20:59:23 -0700

    Explore different type of selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // By tag name:
   selector: 'app-servers',
+  // By attribute:
+  // selector: '[app-servers]',
+  // By class:
+  // selector: '.app-servers',
   // templateUrl: './servers.component.html',
   template: `
     <app-server></app-server>
