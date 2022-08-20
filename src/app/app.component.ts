51780a51588612b4b49e56ad179a33eff24d3eb8
Grace <wangxiaotian2012@gmail.com>
2022-08-19 18:59:22 -0700

    Explore how the styles and styleUrls property works for a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // Regardless of the order in the object,
+  // the <style> for the styles property is always after the <style> for styleUrls
+  // therefore "styles" always takes precedence over "styleUrls"
+  // Bad practice to use both - always choose one
+  // styles:[`
+  //   h1 {
+  //     color: #5f9ea0;
+  //   }
+  // `],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 }
