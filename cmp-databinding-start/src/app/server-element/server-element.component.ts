86fbd0f5e2fa3398b3d6c4abebada6e55efc337b
Grace <wangxiaotian2012@gmail.com>
2022-09-14 17:24:35 -0700

    Add notes about view encapsulation

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "app-server-element",
   templateUrl: "./server-element.component.html",
   styleUrls: ["./server-element.component.css"],
+  // ViewEncapsulation: Default Emulated,
+  // can be changed to none which removes the component ng attribute for html tags
+  // encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent implements OnInit {
   @Input("srvElement") element: { type: string; name: string; content: string };
