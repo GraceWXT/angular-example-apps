c4471c93545bd1954eb94153aa21db40036c2f12
Grace <wangxiaotian2012@gmail.com>
2022-09-17 18:41:19 -0700

    Add example of Injectable providedIn syntax instead of providers

--- services-start/src/app/app.module.ts
+++ services-start/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
 import { AccountsService } from './shared/accounts.service';
-import { LoggingService } from './shared/logging.service';
+// import { LoggingService } from './shared/logging.service';
 
 @NgModule({
   declarations: [AppComponent, AccountComponent, NewAccountComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [AccountsService, LoggingService],
+  providers: [
+    AccountsService
+    // LoggingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
