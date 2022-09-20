46c98d4d33ee70db1a0caed4ba8025883ac743df
Grace <wangxiaotian2012@gmail.com>
2022-09-19 19:14:12 -0700

    Add example of wildcard catch all route

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -19,7 +20,6 @@ const appRoutes: Routes = [
     component: UsersComponent,
     children: [{ path: ':id/:name', component: UserComponent }]
   },
-
   {
     path: 'servers',
     component: ServersComponent,
@@ -27,7 +27,10 @@ const appRoutes: Routes = [
       { path: ':id', component: ServerComponent },
       { path: ':id/edit', component: EditServerComponent }
     ]
-  }
+  },
+  { path: 'not-found', component: PageNotFoundComponent },
+  // Wildcard catch all & redirect: make sure wildcard is the last in the routes array
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -38,7 +41,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [ServersService],
