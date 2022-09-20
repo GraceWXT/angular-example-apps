90cc7ed56a7b419f6ae5e910c3329a1f3186eec3
Grace <wangxiaotian2012@gmail.com>
2022-09-19 19:22:07 -0700

    Add example of home page redirect with pathMatch "full"

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -14,7 +14,8 @@ import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   {
     path: 'users',
     component: UsersComponent,
