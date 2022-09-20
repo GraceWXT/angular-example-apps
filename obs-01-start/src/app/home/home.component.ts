0742acaefa936897bb329f7dfb9d5dc422eb7b84
Grace <wangxiaotian2012@gmail.com>
2022-09-20 15:19:26 -0700

    Add example of observable error handling

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -17,12 +17,20 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
         count++;
       }, 1000);
     });
 
-    this.customIntervalSubscription = customIntervalObs.subscribe((data) =>
-      console.log('data', data)
+    this.customIntervalSubscription = customIntervalObs.subscribe(
+      (data) => {
+        console.log('data', data);
+      },
+      (error) => {
+        alert(`${error.message}`);
+      }
     );
   }
 
