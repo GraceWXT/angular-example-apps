55e2196df85fde9cf58a5971a8197b6963617129
Grace <wangxiaotian2012@gmail.com>
2022-09-20 20:06:29 -0700

    Add example of custom validator

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -9,11 +9,15 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  invalidUsernames = ['chris', 'anna'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [
+          Validators.required,
+          this.invalidNames.bind(this)
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email])
       }),
       gender: new FormControl('female'),
@@ -35,5 +39,15 @@ export class AppComponent implements OnInit {
   get hobbyControls() {
     // return (<FormArray>this.signupForm.get('hobbies')).controls;
     return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+
+  invalidNames(control: FormControl): { [s: string]: boolean } {
+    // For browsers not supporting .includes, use .indexOf(value) !== -1
+    if (this.invalidUsernames.includes(control.value)) {
+      return { invalidUsername: true };
+    }
+    // Must return object with key: value - true, or Null
+    // Should not return false
+    return null;
   }
 }
