c11e2600ddc7dba230b71595ad23d98dd2cb9e3f
Grace <wangxiaotian2012@gmail.com>
2022-09-20 23:36:40 -0700

    Add example of adding query params tp the http url

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
@@ -31,11 +31,17 @@ export class PostsService {
   }
 
   fetchPosts() {
+    // Alternative way, possible to append multiple times with different key-value pairs
+    // let searchParams = new HttpParams();
+    // searchParams = searchParams.append('print', 'pretty');
+    // searchParams = searchParams.append('custom-key', 'value');
     return this.http
       .get<{ [key: string]: Post }>(`${baseUrl}/posts.json`, {
         headers: new HttpHeaders({
           'custom-header': 'Hello'
-        })
+        }),
+        params: new HttpParams().set('print', 'pretty')
+        // params: searchParams
       })
       .pipe(
         map((resData) => {
