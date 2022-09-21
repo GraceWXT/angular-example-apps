a2503db7d545e6d066e9833bdf97d2a1b5bd7a88
Grace <wangxiaotian2012@gmail.com>
2022-09-20 21:01:08 -0700

    Add example  of adding parameters to custom pipes

--- pipes-start/src/app/shorten.pipe.ts
+++ pipes-start/src/app/shorten.pipe.ts
@@ -2,9 +2,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'shorten' })
 export class ShortenPipe implements PipeTransform {
-  transform(value: any, ...args: any[]) {
-    if (value.length > 10) {
-      return value.substr(0, 10) + '...';
+  transform(value: any, limit: number, suffix: string) {
+    if (value.length > limit) {
+      return value.substr(0, limit) + suffix;
     }
     return value;
   }
