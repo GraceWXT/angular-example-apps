ef99c5f059867c109fca59a7bf9bc0a4bd5b8f58
Grace <wangxiaotian2012@gmail.com>
2022-09-21 00:10:55 -0700

    Add example of appending headers using an interceptor

--- http-01-start/src/app/auth-interceptor.service.ts
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -14,7 +14,15 @@ export class AuthInterceptorService implements HttpInterceptor {
     // Add conditions to only intercept certain requests
     // e.g. if (req.url...)
     console.log('Request being intercepted...');
+
+    // req is an immutable object
+    const modifiedReq = req.clone({
+      // url: ''
+      headers: req.headers.append('auth-key', 'value')
+      // params: req.params.append('key', 'value')
+    });
+
     // To continue sending the request
-    return next.handle(req);
+    return next.handle(modifiedReq);
   }
 }
