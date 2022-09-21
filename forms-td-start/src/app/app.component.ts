dbe347a62082e969a86b21160495314ff5e3b5a2
Grace <wangxiaotian2012@gmail.com>
2022-09-20 17:42:52 -0700

    Add example of submit and using form data

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -13,6 +13,14 @@ export class AppComponent {
   // Two-way Binding
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -36,6 +44,12 @@ export class AppComponent {
   // An alternative way of getting access to the form
   // Possible to access ViewChild form value when it's not submitted yet
   onSubmit() {
-    console.log('this.form', this.form);
+    // console.log('this.form', this.form);
+    this.submitted = true;
+    this.user.username = this.form.value.userData.username;
+    this.user.email = this.form.value.userData.email;
+    this.user.secretQuestion = this.form.value.secret;
+    this.user.answer = this.form.value.questionAnswer;
+    this.user.gender = this.form.value.gender;
   }
 }
